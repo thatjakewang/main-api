5e8632e03cbb502099f65f83ffa478c0e1e2b93c
JAKE <jake@JAKEs-MacBook-Pro.local>
2026-05-09 07:59:00 +0800

    Add CORS support for Tesla API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from app.routers import tesla
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="My Tesla Analytics API",
     version="0.1.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://jakewang.dev",
+        "https://www.jakewang.dev",
+        "http://127.0.0.1:5001",
+        "http://localhost:5001",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
